00dd85d847c6e3b87a5b9ecfb03bb7e3b10367f3
JordanPavlev <danipavlev@gmail.com>
2022-09-27 22:42:40 +0300

    added env data and --save-dev

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  env: {
+    X_RAPIDAPI_BASE : process.env.X_RAPIDAPI_BASE,
+    X_RAPIDAPI_KEY : process.env.X_RAPIDAPI_KEY,
+    X_RAPIDAPI_HOST : process.env.X_RAPIDAPI_HOST
+  }
 }
 
 module.exports = nextConfig
